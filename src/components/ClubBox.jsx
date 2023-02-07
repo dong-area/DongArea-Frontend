ceadd4d7638b3d8b522cca848dea14b3cc6631c5
bae0203 <gmbae06@dgsw.hs.kr>
2023-02-08 00:43:01 +0900

    ADD :: MainClubBox + Dummy

--- src/components/ClubBox.jsx
+++ src/components/ClubBox.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import * as S from "../style/ClubBoxStyle";
+import Icon from "../asset/Icon/image1.png";
 
-const ClubBox = () => {
+const ClubBox = (props) => {
   return (
     <>
-      <S.MainBox>ClubBox</S.MainBox>
+      <S.MainBox>
+        <S.ImgIcon src={Icon} alt="dd" />
+        <S.DetailTitle>{props.data.titleDetail}</S.DetailTitle>
+        <S.Context>"{props.data.context}"</S.Context>
+        <S.MainTitle>{props.data.title}</S.MainTitle>
+      </S.MainBox>
     </>
   );
 };
--- src/style/ClubBoxStyle.jsx
+++ src/style/ClubBoxStyle.jsx
@@ -1,9 +1,43 @@
 import styled from "styled-components";
 
 export const MainBox = styled.div`
-  width: 25%;
+  width: 20rem;
   height: 12.5rem;
-  background-color: gray;
+  background-color: white;
   margin: 0 5rem 3rem 0rem;
   float: left;
+  border-radius: 0.25rem;
+  position: relative;
+`;
+
+export const DetailTitle = styled.p`
+  font-size: 1rem;
+  coolor: #000;
+  margin: 0.5rem 1rem;
+`;
+
+export const Context = styled.div`
+  font-size: 1.25rem;
+  color: #e7aa4a;
+  margin: 0rem 1rem;
+  width: 60%;
+  word-break: break-all;
+`;
+
+export const MainTitle = styled.nav`
+  bottom: 0;
+  background-color: #e7aa4a;
+  position: absolute;
+  padding: 0.4rem 1rem;
+  font-size: 1.6rem;
+  border-radius: 0.25rem;
+  box-shadow: 0.3rem 0.3rem 0.3rem rgba(0, 0, 0, 0.2);
+`;
+
+export const ImgIcon = styled.img`
+  width: 6.25rem;
+  height: 6.25rem;
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
 `;
