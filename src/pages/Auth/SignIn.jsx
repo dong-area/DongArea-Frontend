32ffbcd30e05fc74b380b40f200e94ee6686e69e
sueeeeh <olivila0501@gmail.com>
2023-02-07 12:07:13 +0900

    Update SignIn

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,7 +1,11 @@
 import axios from 'axios'
+import React, { useState } from "react";
 
 //로그인
 const SignIn = () => {
+    const [Id,setId] = useState('')
+    const [Pw,setPw] = useState('')
+
     const onSubmit = () => {
         axios.post('링크',{
             //id, password
@@ -15,8 +19,8 @@ const SignIn = () => {
     return(
         <>
             <form onSubmit={onSubmit}>
-                <input className="id" placeholder="Id"/>
-                <input className="password" placeholder="비밀번호"/>
+                <input className="id" placeholder="Id" value={Id} onChange={(e)=>setId(e.target.value)}/>
+                <input className="password" placeholder="비밀번호" value={Pw} onChange={(e)=>setPw(e.target.value)}/>
                 <button type="submit">Sign In</button>
             </form>
         </>
