89a0e2d0fa37398bbddf6cc1c96dfd470e36dad6
bae0203 <gmbae06@dgsw.hs.kr>
2023-02-07 11:28:36 +0900

    ADD :: UseNavigate + isToken?
    토큰 유무로 로그인 회원가입 띄우기
    + useNavigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "../style/HeaderStyle";
 
 const Header = () => {
+  const [isToken, setIsToken] = useState(false);
+  const Navigate = useNavigate();
   return (
     <S.Header>
       <S.ContentSpan>D.A</S.ContentSpan>
@@ -9,8 +12,27 @@ const Header = () => {
         <S.ContentTitleSpan color={`#fff`}>개설</S.ContentTitleSpan>
         <S.ContentTitleSpan color={`#fff`}>프로젝트</S.ContentTitleSpan>
         <S.ContentTitleSpan color={`#fff`}>콜라보</S.ContentTitleSpan>
-        <S.ContentTitleSpan color={`#e7aa4a`}>내 동아리</S.ContentTitleSpan>
-        <S.ContentTitleSpan color={`#e7aa4a`}>내 정보</S.ContentTitleSpan>
+        {isToken ? (
+          <>
+            <S.ContentTitleSpan color={`#e7aa4a`}>내 동아리</S.ContentTitleSpan>
+            <S.ContentTitleSpan color={`#e7aa4a`}>내 정보</S.ContentTitleSpan>
+          </>
+        ) : (
+          <>
+            <S.ContentTitleSpan
+              color={`#e7aa4a`}
+              onClick={() => Navigate("/SignIn")}
+            >
+              로그인
+            </S.ContentTitleSpan>
+            <S.ContentTitleSpan
+              color={`#e7aa4a`}
+              onClick={() => Navigate("/SignUp")}
+            >
+              회원가입
+            </S.ContentTitleSpan>
+          </>
+        )}
       </S.TitleNav>
     </S.Header>
   );
