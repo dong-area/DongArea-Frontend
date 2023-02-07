1d5473d80b173fdead3a9bc82ec5802304feb0cd
sueeeeh <olivila0501@gmail.com>
2023-02-07 10:03:30 +0900

    Update Auth
    before Backend

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,7 +1,26 @@
+import axios from 'axios'
 
-
+//로그인
 const SignIn = () => {
+    const onSubmit = () => {
+        axios.post('링크',{
+            //id, password
+        })
+        .then((res,req)=>{
+            //token
+            alert('로그인 성공')
+        })
+    }
 
+    return(
+        <>
+            <form onSubmit={onSubmit}>
+                <input className="id" placeholder="Id"/>
+                <input className="password" placeholder="비밀번호"/>
+                <button type="submit">Sign In</button>
+            </form>
+        </>
+    )
 }
 
 export default SignIn;
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,7 +1,30 @@
+import axios from 'axios'
 
-
+//회원가입
 const SignUp = () => {
+    const onSubmit = () => {
+        axios.post('링크',{
+            //data
+        })
+        .then((res,req)=>{
+            //token
+            alert('계정 생성 성공')
+        })
+    }
 
+    return(
+        <>
+            <form onSubmit={onSubmit}>
+                <input className="id" placeholder="Id"/>
+                <input className="password" placeholder="비밀번호"/>
+                <input className="name" placeholder="이름"/>
+                <input className="club" placeholder="동아리"/>
+                <input className="class" placeholder="반"/>
+                <input className="number" placeholder="번호"/>
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
+    )
 }
 
 export default SignUp;
