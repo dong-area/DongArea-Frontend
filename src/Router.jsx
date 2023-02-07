38d16afd4fc024df1dc660e9b8cd46a9e595be76
bae0203 <gmbae06@dgsw.hs.kr>
2023-02-07 10:29:27 +0900

    FIX :: Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
-import Auth from "./pages/Auth";
 import MyInfo from "./pages/MyInfo";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
@@ -11,10 +10,9 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
         <Route path="/myinfo" element={<MyInfo />}></Route>
-        <Route path="/SignIn" element={<SignIn/>}></Route>
-        <Route path="/SignUp" element={<SignUp/>}></Route>
+        <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/SignUp" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   );
