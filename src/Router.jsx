fcf53b05ee6b4a20c45b1c98d43044d0b1f3ef82
sueeeeh <olivila0501@gmail.com>
2023-02-07 09:24:37 +0900

    Add Route

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Auth from "./pages/Auth";
 import MyInfo from "./pages/MyInfo";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
 
 const Router = () => {
   return (
@@ -11,6 +13,8 @@ const Router = () => {
         <Route path="/" element={<Main />}></Route>
         <Route path="/auth" element={<Auth />}></Route>
         <Route path="/myinfo" element={<MyInfo />}></Route>
+        <Route path="/SignIn" element={<SignIn/>}></Route>
+        <Route path="/SignUp" element={<SignUp/>}></Route>
       </Routes>
     </BrowserRouter>
   );
