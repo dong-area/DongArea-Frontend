c3b7a907df0f967134db3115a7804a375e1e1690
sueeeeh <olivila0501@gmail.com>
2023-02-08 09:51:35 +0900

    Update Myinfo

--- src/pages/MyInfo.jsx
+++ src/pages/MyInfo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import axios from 'axios';
 import BaseAxios from "../utils/BaseAxios";
+import * as S from "../style/MyinfoStyle";
+import mypageDongari from "../asset/mypagedongari.png";
+import mypageProfile from "../asset/mypageProfile.png";
 
 const MyInfo = (props) => {
-
+  console.log(props.token)
   BaseAxios.get('/authed/user/userinfo',{
     headers : {
-      Authorization : props.Token
+      Authorization : props.token
     }
   })
   .then((res,req)=>{
@@ -15,7 +18,13 @@ const MyInfo = (props) => {
 
   return (
     <>
-      <div>MyInfo</div>
+      <S.Body/>
+
+      <S.LogoWholeDiv>
+        <S.RightImgDiv/>
+        <S.RightImg src={mypageDongari}></S.RightImg>
+      </S.LogoWholeDiv>
+
     </>
   );
 };
